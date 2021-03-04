a54ac221d353c1fd45367fe4ffdef486226494ba
ed1868 <ruizeduardo21@gmail.com>
2021-03-04 16:31:05 -0500

    deploy v2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn to upload react with AI Nomads
         </a>
       </header>
     </div>
